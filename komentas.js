9805b712a02683bd4945eed70d7d8f7eff61135d
SalsabilaNs <127173079+slsabla@users.noreply.github.com>
2023-03-30 11:28:49 +0700

    contoh

---
+++ komentas.js
@@ -0,0 +1,7 @@
+//console.log("Ini adalah Komentar Satu Baris");
+console.log("Comments //");
+
+/*console.log("Ini Merupakan komentar dengan satu baris");
+console.log("Teks apapun yang berada disini akan dijadikan komentar");
+console.log("Ketika Menggunakan ini jangan lupa untuk menutupnya");*/
+console.log("Comments /* */");
